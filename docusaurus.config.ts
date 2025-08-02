14f762529d3432adb64ec8c4f53233991afd316f
Smidigstorm <arnehenrik.storm@gmail.com>
2025-08-02 18:24:24 +0200

    Revert URL config - keep GitHub Pages URL for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://laug.smidigakademiet.no',
+  url: 'https://SmidigStorm.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/laug-docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
